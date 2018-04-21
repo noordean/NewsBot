7ead409a4d6b4f92852bd06e740c60440034121d
noordean <ebroyeem90@gmail.com>
2018-04-21 10:25:53 +0100

    feat(botResponse): implement bot response
    - create bot responses based on with response
    - train bot to understand more statements
    
    [Delivers]

--- app/controllers/bots_controller.rb
+++ app/controllers/bots_controller.rb
@@ -1,4 +1,6 @@
 class BotsController < ApplicationController
+  include BotResponse
+
   def index
     scraped_data = CrawlerService.new("http://dailypost.ng/hot-news/").scrape
     Politics.transaction do
@@ -8,5 +10,21 @@ class BotsController < ApplicationController
 
   def message
     wit_response = WitService.new.client.message(params[:message].strip)
+    render json: bot_response_message(wit_response)
+  end
+
+  private
+
+  def bot_response_message(wit_response)
+    wit_entities = wit_response["entities"]
+    if !wit_entities.empty?
+      if wit_entities["intent"]
+        { message: responses[wit_entities["intent"][0]["value"].to_sym] }
+      elsif wit_entities["message"]
+        { message: Politics.search(wit_entities["message"][0]["value"]) }
+      end
+    else
+      { message: "I do not understand that. You can ask me for news and I'll provide you. An example could be: 'Get me some news on Buhari'"}
+    end
   end
 end
